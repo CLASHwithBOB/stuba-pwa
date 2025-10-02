6447a974a9994a61482a877fa74f00e4cc54dd7f
Преслав Стоянов <85369896+Preslav-Stoyanov@users.noreply.github.com>
2025-10-02 22:41:31 +0200

    PWA-7: Design a register page (#4)
    * PWA-7: Moved register page to auth subfolder
    
    * PWA-7: Designed register page
    
    * PWA-7: Removed useless actions

--- src/pages/RegisterPage.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <q-page class="row items-center justify-evenly">
-    <h1>Register Page</h1>
-  </q-page>
-</template>
-
-<script setup lang="ts">
-//
-</script>
---
+++ src/pages/auth/RegisterPage.vue
@@ -0,0 +1,91 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const nickname = ref('');
+const email = ref('');
+const password = ref('');
+const passwordConfirm = ref('');
+const displayPassword = ref(false);
+const displayPasswordConfirm = ref(false);
+</script>
+<template>
+  <q-page class="row items-center justify-center">
+    <q-card class="w-full">
+      <q-card-section>
+        <div class="text-h6">Sign up</div>
+        <div class="text-subtitle2 text-grey-6">Enter your information to create an account</div>
+      </q-card-section>
+
+      <q-separator />
+
+      <q-card-section>
+        <form class="q-gutter-md">
+          <q-input v-model="nickname" label="Nickname" outlined dense />
+          <q-input type="email" v-model="email" label="Email" outlined dense />
+          <q-input
+            v-model="password"
+            :type="displayPassword ? 'text' : 'password'"
+            label="Password"
+            outlined
+            dense
+          >
+            <template v-slot:append>
+              <q-icon
+                :name="displayPassword ? 'visibility_off' : 'visibility'"
+                class="cursor-pointer"
+                @click="displayPassword = !displayPassword"
+              />
+            </template>
+          </q-input>
+          <q-input
+            v-model="passwordConfirm"
+            label="Confirm Password"
+            :type="displayPasswordConfirm ? 'text' : 'password'"
+            outlined
+            dense
+          >
+            <template v-slot:append>
+              <q-icon
+                :name="displayPasswordConfirm ? 'visibility_off' : 'visibility'"
+                class="cursor-pointer"
+                @click="displayPasswordConfirm = !displayPasswordConfirm"
+              />
+            </template>
+          </q-input>
+        </form>
+      </q-card-section>
+
+      <q-card-actions vertical class="q-px-md">
+        <q-btn color="primary">Create an account</q-btn>
+      </q-card-actions>
+
+      <q-card-section class="text-center">
+        <p class="text-grey-6">
+          Already have an account?
+          <router-link to="/login" class="text-primary">Log in</router-link>
+        </p>
+      </q-card-section>
+    </q-card>
+  </q-page>
+</template>
+<style scoped>
+.q-page {
+  align-items: start;
+}
+
+.q-card {
+  width: 100%;
+  height: 100vh;
+}
+
+@media (min-width: 480px) {
+  .q-page {
+    align-items: center;
+  }
+
+  .q-card {
+    width: auto;
+    height: auto;
+  }
+}
+</style>
