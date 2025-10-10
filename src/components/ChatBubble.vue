f8b1a307fc605d1eeb15ff55ca5e80bde6807042
Boris Semov <boris2003semov@gmail.com>
2025-10-11 00:56:27 +0200

    PWA-14: Created Dashboard page

---
+++ src/components/ChatBubble.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+defineProps<{
+  user: { nickname: string };
+  text: string[];
+  sent?: boolean;
+}>();
+</script>
+<template>
+  <q-chat-message
+    :text
+    :sent
+    :text-color="sent ? 'white' : 'black'"
+    :bg-color="sent ? 'primary' : 'amber'"
+  >
+    <template v-slot:name>{{ user.nickname }}</template>
+    <template v-slot:stamp>7 minutes ago</template>
+    <template v-slot:avatar>
+      <img
+        class="q-message-avatar q-message-avatar--sent"
+        src="https://cdn.quasar.dev/img/avatar4.jpg"
+      />
+    </template>
+  </q-chat-message>
+</template>
