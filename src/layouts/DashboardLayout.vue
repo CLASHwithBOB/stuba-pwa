898d35e80ff9372f4def6b7ea3a4e1651f8c6cfe
Преслав Стоянов <85369896+Preslav-Stoyanov@users.noreply.github.com>
2025-10-24 14:34:50 +0200

    PWA-75: Display channels instead of placeholders
    * PWA-75: Removed a useless div
    
    * PWA-75: Listing backend channels instead of fake ones

--- src/layouts/DashboardLayout.vue
+++ src/layouts/DashboardLayout.vue
@@ -4,7 +4,7 @@ import ChannelItem from 'src/components/ChannelItem.vue';
 import OptionsDropdown from 'src/components/OptionsDropdown.vue';
 import UserDropdown from 'src/components/UserDropdown.vue';
 import { useChannels } from 'src/stores/channels';
-import { computed, onMounted, ref } from 'vue';
+import { computed, onMounted } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
 
 const $q = useQuasar();
@@ -19,25 +19,6 @@ const mobileView = computed(() => (selectedChannelId.value ? 'chat' : 'list'));
 onMounted(async () => {
   if (!channelStore.channels?.length) await channelStore.loadChannels();
 });
-
-const channels = ref([
-  { id: 1, name: 'Channel 1' },
-  { id: 2, name: 'Channel 2' },
-  { id: 3, name: 'Channel 3' },
-  { id: 4, name: 'Channel 4' },
-  { id: 5, name: 'Channel 5' },
-  { id: 6, name: 'Channel 6' },
-  { id: 7, name: 'Channel 7' },
-  { id: 8, name: 'Channel 8' },
-  { id: 9, name: 'Channel 9' },
-  { id: 10, name: 'Channel 10' },
-  { id: 11, name: 'Channel 11' },
-  { id: 12, name: 'Channel 12' },
-  { id: 13, name: 'Channel 13' },
-  { id: 14, name: 'Channel 14' },
-  { id: 15, name: 'Channel 15' },
-  { id: 16, name: 'Channel 16' },
-]);
 </script>
 
 <template>
@@ -70,20 +51,11 @@ const channels = ref([
         >
           <q-scroll-area class="col">
             <q-list>
-              <ChannelItem
-                v-for="channel in channels"
-                :key="channel.id"
-                :channel="{
-                  id: channel.id,
-                  name: channel.name,
-                }"
-              />
+              <ChannelItem v-for="channel in channelStore.channels" :key="channel.id" :channel />
             </q-list>
           </q-scroll-area>
 
-          <div>
-            <UserDropdown />
-          </div>
+          <UserDropdown />
         </q-page>
 
         <router-view v-slot="{ Component }">
